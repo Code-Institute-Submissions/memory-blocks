7dcb02aa3b747e5e4cfa424b87efc7d3a8912148
Johnny-Morgan <jwjmorgan@gmail.com>
2021-01-31 16:10:12 +0000

    Add text animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -320,3 +320,26 @@ $("#play-again").click(function () {
         }
     }
 })
+
+
+/* Credit for text animation - Tobias Ahlin
+   (https://tobiasahlin.com/moving-letters/#9)
+*/
+// Wrap every letter in a span
+let textWrapper = document.querySelector('.ml9 .letters');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline()
+    .add({
+        targets: '.ml9 .letter',
+        scale: [0, 1],
+        duration: 1500,
+        elasticity: 600,
+        delay: (el, i) => 45 * (i + 1)
+    }).add({
+        targets: '.ml9',
+        // opacity: 0,
+        duration: 1000,
+        easing: "easeOutExpo",
+        delay: 1000
+    });
