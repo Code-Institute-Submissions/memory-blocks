441178876867e96c997d3211c446555132e151fb
Johnny-Morgan <jwjmorgan@gmail.com>
2021-02-03 18:03:58 +0000

    Add calculatePlayingTime function testing

--- assets/jasmine_testing/spec/memoryBlocksSpec.js
+++ assets/jasmine_testing/spec/memoryBlocksSpec.js
@@ -1,10 +1,12 @@
 describe("game timers", () => {
+
+    beforeEach(() => {
+        easyGame = false;
+        mediumGame = false;
+        hardGame = false;
+    });
+
     describe("memory time function", () => {
-        beforeEach(() => {
-            easyGame = false;
-            mediumGame = false;
-            hardGame = false;
-        });
         it("should return 10", () => {
             easyGame = true;
             const result = calculateMemorizingTime();
@@ -19,6 +21,24 @@ describe("game timers", () => {
             hardGame = true;
             const result = calculateMemorizingTime();
             expect(result).toBe(30);
+        });
+    });
+
+    describe("playing time function", () => {
+        it("should return 20", () => {
+            easyGame = true;
+            const result = calculatePlayingTime();
+            expect(result).toBe(20);
+        });
+        it("should return 40", () => {
+            mediumGame = true;
+            const result = calculatePlayingTime();
+            expect(result).toBe(40);
+        });
+        it("should return 60", () => {
+            hardGame = true;
+            const result = calculatePlayingTime();
+            expect(result).toBe(60);
         });
     });
 });
