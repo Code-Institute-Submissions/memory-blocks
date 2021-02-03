ac50b6a54095fd56cb0082a54bc1ee9c5a0d1a6e
Johnny-Morgan <jwjmorgan@gmail.com>
2021-02-03 16:26:07 +0000

    Add calculateMemorizingTime function testing

--- assets/jasmine_testing/spec/memoryBlocksSpec.js
+++ assets/jasmine_testing/spec/memoryBlocksSpec.js
@@ -1,2 +1,26 @@
-// Tests go here
+describe("game timers", () => {
+    describe("memory time function", () => {
+        beforeEach(() => {
+            easyGame = false;
+            mediumGame = false;
+            hardGame = false;
+        });
+        it("should return 10", () => {
+            easyGame = true;
+            const result = calculateMemorizingTime();
+            expect(result).toBe(10);
+        });
+        it("should return 20", () => {
+            mediumGame = true;
+            const result = calculateMemorizingTime();
+            expect(result).toBe(20);
+        });
+        it("should return 30", () => {
+            hardGame = true;
+            const result = calculateMemorizingTime();
+            expect(result).toBe(30);
+        });
+    });
+});
+
 
